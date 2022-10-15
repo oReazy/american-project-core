93b9832fc0d8c59c4449e6ef90a467a407e82a3b
Reazy <oreazy.official@gmail.com>
2022-10-15 17:43:15 +0300

    Обновление экскурсии

--- states.py
+++ states.py
@@ -1,17 +1,17 @@
-# ----------------------------------------------------------------------------------------------------------------------
+# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 # Игровой Role-Play чат-бот для ВКонтакте.
 #
 # Автор: Reazy, 2022 год.
 
-# ----------------------------------------------------------------------------------------------------------------------
+# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
-import asyncio, ast, states, json, time, datetime, random, traceback
+import asyncio, ast, json, time, datetime, random, traceback
 from vkbottle.bot import Bot, Message, MessageEvent
 from vkbottle import API, LoopWrapper, GroupEventType
 
 from modules import database, registration
-# ----------------------------------------------------------------------------------------------------------------------
+# ——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
 
 STATES = {'registration.registration_1': registration.registration_1,
           'registration.registration_1_check': registration.registration_1_check,
@@ -19,4 +19,21 @@ STATES = {'registration.registration_1': registration.registration_1,
           'registration.registration_2_man': registration.registration_2_man,
           'registration.registration_2_woman': registration.registration_2_woman,
           'registration.registration_3': registration.registration_3,
+          'registration.registration_3_check': registration.registration_3_check,
+          'registration.registration_4': registration.registration_4,
+          'registration.registration_4_check': registration.registration_4_check,
+          'registration.registration_5': registration.registration_5,
+          'registration.registration_5_friend': registration.registration_5_friend,
+          'registration.registration_5_list_chatbot': registration.registration_5_list_chatbot,
+          'registration.registration_5_search': registration.registration_5_search,
+          'registration.registration_5_youtube': registration.registration_5_youtube,
+          'registration.registration_5_other': registration.registration_5_other,
+          'registration.registration_6': registration.registration_6,
+          'registration.registration_6_accept': registration.registration_6_accept,
+          'registration.registration_6_denial': registration.registration_6_denial,
+          'registration.registration_7': registration.registration_7,
+          'registration.registration_8': registration.registration_8,
+          'registration.registration_9': registration.registration_9,
+          'registration.registration_9_1': registration.registration_9_1,
+          'registration.newAccaunt': registration.newAccaunt,
         }
